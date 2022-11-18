1bd35102c6c7b716f8b7d2ad2b5bc424b8b49c2e
dangquanghusc <76767815+dangquanghusc@users.noreply.github.com>
2022-11-18 21:06:20 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc em iu của anh có một ngày sinh nhật tràn ngập niềm vui và nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const giftImage = 'em.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
